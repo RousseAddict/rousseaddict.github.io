815305cd3835d06e621ca56696167b4d0c077d87
RousseAddict <rousseaddict@gmail.com>
2015-09-22 22:55:42 +0200

    mobile version 1.3

--- mobile/script.js
+++ mobile/script.js
@@ -4,8 +4,8 @@ loaded index.html version mobile
 
 //ajuster la taille de l'image principale à la taille de l'écran
 var larg = (window.innerWidth);
-$('.container').css('width',(larg-20)+"px");
-
+$('.container').css('width',(larg-50)+"px");
+$('.container img').css('width',(larg-55)+"px");
 //ajuster la taille du menu à la moitié de la taille de l'écran
 var haut = (window.innerHeight);
 $('.nav2').css('width',(larg/2)+"px");
