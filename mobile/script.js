1fa938ae1e9317a7b60cf9019cf42169c9b1f05d
RousseAddict <rousseaddict@gmail.com>
2015-09-22 23:07:56 +0200

    mobile version 1.3

--- mobile/script.js
+++ mobile/script.js
@@ -9,7 +9,7 @@ $('.container img').css('width',(larg-55)+"px");
 //ajuster la taille du menu à la moitié de la taille de l'écran
 var haut = (window.innerHeight);
 $('.nav2').css('width',(larg/2)+"px");
-$('.nav2').css('right',(-larg/2)+"px");
+//$('.nav2').css('right',(-larg/2)+"px");
 $('.nav2').css('height',(haut-50)+"px");
 /*
 functions
@@ -22,10 +22,10 @@ function randomImg(){
 
 //gère l'action du bouton du menu
 $( "#BoutonMenu" ).click(function() {
-  $( ".nav2" ).css('right','0');
+ // $( ".nav2" ).css('right','0');
   $( ".nav2" ).css('display','block');
 });
 $( '.container' ).click(function() {
-  $('.nav2').css('right',(-larg/2)+"px");
+//  $('.nav2').css('right',(-larg/2)+"px");
   $( ".nav2" ).css('display','none');
 });
