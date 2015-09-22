7d236a932783a881d16afb59fb1fb6159826b7d7
RousseAddict <rousseaddict@gmail.com>
2015-09-22 22:22:34 +0200

    mobile version 1.2

--- mobile/script.js
+++ mobile/script.js
@@ -1,3 +1,25 @@
+/*
+loaded index.html version mobile
+*/
+
+//ajuster la taille de l'image principale à la taille de l'écran
+var larg = (window.innerWidth);
+$('.container').css('max-width',larg+"px");
+
+//ajuster la taille du menu à la moitié de la taille de l'écran
+var haut = (window.innerHeight);
+$('.nav2').css('width',(larg/2)+"px");
+$('.nav2').css('height',(haut-50)+"px");
+/*
+functions
+*/
+
+//génère une image aléatoire parmis la liste des images diponibles
 function randomImg(){
    document.getElementById('image').src = "../rousses/" + Math.round(Math.random()*70) + ".jpg";
+}
+
+//gère l'action du bouton du menu
+function menuO(){
+	document.getElementByClassName('nav2')[0].style.right = '0';
 }
